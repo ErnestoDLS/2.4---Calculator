836ffde11c207ca1598e84b987c87193f43ff666
Ernesto De Los Santos <netos_id@yahoo.com>
2016-04-24 21:31:15 -0500

    Unfinished

---
+++ JS/main.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(e){
+  var calNum = document.querySelector("[data-js='num-Input']")
+  var calcDisplay = document.querySelector("[data-js='cal-output']")
+}
