48b54e7bd381701798fabd1784363aa927245b57
Ernesto De Los Santos <netos_id@yahoo.com>
2016-04-27 15:58:46 -0500

    fixed the sass errors.

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,1 @@
-document.addEventListener("DOMContentLoaded", function(e){
-  var calNum = document.querySelector("[data-js='num-Input']")
-  var calcDisplay = document.querySelector("[data-js='cal-output']")
-}
+document.addEventListener("DOMContentLoaded", function(e){})
